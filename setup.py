b0a8c10cd43138802178ba4acfb1a18b2adac818
Ken Kundert <ken@theKunderts.net>
2017-07-18 16:06:16 -0700

    bump version to 2.0.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ keywords='''
 
 setup(
     name='quantiphy',
-    version='2.0.5',
+    version='2.0.6',
     description='physical quantities (numbers with units)',
     long_description=readme,
     author="Ken Kundert",
